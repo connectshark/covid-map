a753a258c5759d87b7f29053c32a560d67e1931f
connectshark <bobhus394@gmail.com>
2021-07-30 00:11:14 +0800

    修改連線api設定

--- src/hook/api.js
+++ src/hook/api.js
@@ -3,8 +3,16 @@ import axios from 'axios'
 const api = {
   getFullData () {
     return new Promise((resolve) => {
-      axios.get(process.env.VUE_APP_URL)
-        .then(res => resolve(res.data))
+      axios({
+        method: 'get',
+        baseURL: process.env.VUE_APP_BASE_URL,
+        url: process.env.VUE_APP_URL,
+        params: {
+          CK: 'covid-19@nchc.org.tw',
+          querydata: '4001',
+          limited: 'TWN'
+        }
+      }).then(res => resolve(res.data))
     })
   }
 }
