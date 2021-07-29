a9a25683689f99aaba42a34fb4c92063948e4eaf
connectshark <bobhus394@gmail.com>
2021-07-29 22:05:16 +0800

    環境變數

--- src/hook/api.js
+++ src/hook/api.js
@@ -3,10 +3,9 @@ import axios from 'axios'
 const api = {
   getFullData () {
     return new Promise((resolve) => {
-      axios.get('/api/covid19?CK=covid-19@nchc.org.tw&querydata=3001&limited=TWN')
+      axios.get(process.env.VUE_APP_URL)
         .then(res => resolve(res.data))
     })
-    
   }
 }
 
