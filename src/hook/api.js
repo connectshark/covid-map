36db5ef9a0cb7d306f63bb7a94624a7fa07f9e1c
connectshark <bobhus394@gmail.com>
2021-07-29 22:03:54 +0800

    連接api

--- src/hook/api.js
+++ src/hook/api.js
@@ -3,17 +3,8 @@ import axios from 'axios'
 const api = {
   getFullData () {
     return new Promise((resolve) => {
-      axios({
-        method: 'get',
-        baseURL: process.env.VUE_APP_URL,
-        params: {
-          CK: 'covid-19@nchc.org.tw',
-          querydata: '4001',
-          limited: 'TWN'
-        }
-      }).then(res => {
-        resolve(res.data)
-      })
+      axios.get('/api/covid19?CK=covid-19@nchc.org.tw&querydata=3001&limited=TWN')
+        .then(res => resolve(res.data))
     })
     
   }
